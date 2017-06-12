a17ca6ce8a229903d0afd57cd4f06cb5bfdf851e
Eduardo <Eduardo@DESKTOP-K9OT8MK>
2017-06-12 12:01:04 +0200

    Anagrams commit

--- src/com/toy/anagrams/ui/Anagrams.java
+++ src/com/toy/anagrams/ui/Anagrams.java
@@ -261,10 +261,10 @@ public class Anagrams extends JFrame {
 
     private void guessedWordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_guessedWordActionPerformed
         if (wordLibrary.isCorrect(wordIdx, guessedWord.getText())){
-            feedbackLabel.setText("Correct! Try a new word!");
+            feedbackLabel.setText("Correcto! Prueba con una nueva palabra!");
             getRootPane().setDefaultButton(nextTrial);
         } else {
-            feedbackLabel.setText("Incorrect! Try again!");
+            feedbackLabel.setText("Incorrecto! Prueba de nuevo!");
             guessedWord.setText("");
         }
 
